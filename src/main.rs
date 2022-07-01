b08b20bf9052989b5b50c911886ef027cb00cda9
zhangyuxuan <zhangyuxuan01@gmail.com>
2022-07-02 03:19:22 +0800

    数据库池替换为SeaORM

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,58 @@
-use std::{net::SocketAddr, string};
-
+use std::{net::SocketAddr, io::{self, Read}};
 use axum::{
-    http::{HeaderValue, Method},
     response::Html,
     routing::{get, post},
     Json, Router,
 };
-use jsonwebtoken::{decode, encode};
+use jsonwebtoken::{ encode };
 use serde::{Deserialize, Serialize};
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::{CorsLayer};
+
+#[derive(Debug, Clone, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "categoies")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
+    pub id: i32,
+    pub name: String,
+    pub is_del: bool,
+}
+
+
 
 const SECRET_KEY: &[u8] = b"secret";
 
-
-
-
 #[tokio::main]
 async fn main() {
+    let cfg = intialize_dbconfig();
+    let client = get_client(cfg).await.unwrap();
+
+    find_by_username(client).await.unwrap();
+
 
     let cors = CorsLayer::permissive();
 
     let api_nest = Router::new().route("/login", post(login_handler));
 
     let app = Router::new()
-    .route("/", get(index_handler))
-    .nest("/api/v1", api_nest)
-    .layer(cors)
-    .into_make_service();
+        .route("/", get(index_handler))
+        .nest("/api/v1", api_nest)
+        .layer(cors)
+        .into_make_service();
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
     println!("Listening on http://{}", addr);
     axum::Server::bind(&addr).serve(app).await.unwrap();
 }
 
-
-
 async fn index_handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
-#[derive(Debug,Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct LoginRequest {
     username: String,
     password: String,
 }
-#[derive(Debug,Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct LoginResponse {
     access_token: String,
     refresh_token: String,
